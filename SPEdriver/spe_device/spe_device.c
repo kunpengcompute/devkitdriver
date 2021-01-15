6f61d20565358e3cc13d4bfed8aa30cc1d892d94
yunanlu0404 <luyunan0404@163.com>
2021-01-15 17:05:02 +0800

    initial SPEdriver

--- SPEdriver/spe_device/spe_device.c
+++ SPEdriver/spe_device/spe_device.c
@@ -1,8 +1,21 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2019-2020. All rights reserved.
+ * Copyright (C) 2020-2021. Huawei Technologies Co., Ltd. All rights reserved.
+
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ 
  * Description: Application class implementation, which controls the whole app start stop and so on.
- * Author: malluma team
- * Create: 2019-12-07
  */
 #include <linux/kconfig.h>
 #include <linux/kernel.h>
